ec9048bbc0648bbe0193fce77701991b887fd5a5
Justin Doyle <jdoyle@donjohnston.com>
2014-05-13 17:20:13 -0500

    commitMessage

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,7 @@
             shell: {
                 commit: {
                     command: function(commitMessage){
-                        grunt.log.writeln(commitMessage);
+                        grunt.log.writeln('<%= commitMessage %>');
                         return "git commit -a -m '" + commitMessage + "'";
                     }
                 },
